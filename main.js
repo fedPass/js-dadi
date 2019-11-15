cc0a626d0ad206714e693f086b7dc6e4bce8a347
fedPass <federica.passanante@gmail.com>
2019-11-15 20:29:57 +0100

    correzzioni

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 // alert('prova');
 // Generare un numero casuale tra 1 e 6.
-var numeroBanco = 
+var numeroBanco = Math.floor(Math.random() * 6) + 1;
+console.log(numeroBanco);
 // Attraverso un prompt chiedere all'utente un numero tra 1 e 6.
+var numeroUtente = prompt ('Digita un numero');
+console.log(numeroUtente);
 // Stampare un messaggio per dire all'utente se ha vinto oppure no (il numero più alto vince).
+if (numeroBanco < numeroUtente) {
+    console.log('Ha vinto!!!');
+} else if (numeroBanco > numeroUtente) {
+    console.log('Ha perso!!!');
+} else {
+    console.log('Parità!!!');
+}
