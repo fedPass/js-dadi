d2d3943074014092254a74b5b74ff84965296732
fedPass <federica.passanante@gmail.com>
2019-11-15 20:03:14 +0100

    inizio es

---
+++ main.js
@@ -0,0 +1,5 @@
+// alert('prova');
+// Generare un numero casuale tra 1 e 6.
+var numeroBanco = 
+// Attraverso un prompt chiedere all'utente un numero tra 1 e 6.
+// Stampare un messaggio per dire all'utente se ha vinto oppure no (il numero più alto vince).
